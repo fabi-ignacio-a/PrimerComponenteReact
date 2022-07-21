04d656244cacf566210e46b99ea5f9aa1d0878c8
fabi_ignacio_ <100646521+fabi-ignacio-a@users.noreply.github.com>
2022-07-21 13:01:36 -0400

    Creacion Primer Componente Se usa compontente Title para empezar a entender como funcionan.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./styles.css";
 
+//Regla de React, cualquier componente de React, se escribe con Mayuscula
+const Title = ({ message, titleClass }) => {
+  //La variable message es una props
+  return <h1 className={`${titleClass} title `}> {message} </h1>;
+};
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Title titleClass="green" message="Me gusta React" />
+      <Title titleClass="blue" message="Me gusta Javascript" />
+      <Title titleClass="salmon" message="Me gusta NestJS" />
     </div>
   );
 }
